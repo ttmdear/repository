e7d5804ed7df9f3275ad694cdb60452f42295d51
Paweł Bobryk <bobryk.pawel@gmail.com>
2020-08-21 13:05:20 +0200

    Change

--- java/caffeine/src/main/java/com/repo/caffeine/User.java
+++ java/caffeine/src/main/java/com/repo/caffeine/User.java
@@ -1,4 +1,34 @@
 package com.repo.caffeine;
 
 public class User {
+    private final int id;
+
+    private String firstName;
+    private String lastName;
+
+    public User(int id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- java/serialization/src/main/java/com/repo/serialization/PersistentAnimation.java
+++ java/serialization/src/main/java/com/repo/serialization/PersistentAnimation.java
@@ -1,4 +1,49 @@
 package com.repo.serialization;
 
-public class PersistentAnimation {
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class PersistentAnimation implements Serializable, Runnable {
+    private int animationSpeed;
+    private transient Thread animator;
+    private boolean stop = false;
+
+    public PersistentAnimation(int animationSpeed) {
+        this.animationSpeed = animationSpeed;
+
+        animator = new Thread(this);
+        animator.start();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        // our "pseudo-constructor"
+        in.defaultReadObject();
+        // now we are a "live" object again, so let's run rebuild and start
+
+        animator = new Thread(this);
+        animator.start();
+    }
+
+    public void run() {
+        while (!stop) {
+            System.out.println("Animation speed " + animationSpeed);
+
+            try {
+                Thread.sleep(animationSpeed);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void stop() {
+        stop = true;
+    }
+
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
 }
