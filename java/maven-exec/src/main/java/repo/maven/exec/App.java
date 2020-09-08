5c240f5bf2f4e87a01ad7b121ce11f7a07ad7c02
Paweł Bobryk <bobryk.pawel@gmail.com>
2020-09-08 11:30:13 +0200

    Maven exec plugin

---
+++ java/maven-exec/src/main/java/repo/maven/exec/App.java
@@ -0,0 +1,7 @@
+package repo.maven.exec;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World App!");
+    }
+}
---
+++ java/maven-exec/src/main/java/repo/maven/exec/TestApp.java
@@ -0,0 +1,10 @@
+package repo.maven.exec;
+
+import java.util.Arrays;
+
+public class TestApp {
+    public static void main(String[] args) {
+        System.out.println("Hello World TestApp!");
+        System.out.println("args " + Arrays.asList(args));
+    }
+}
