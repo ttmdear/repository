61791c674add41d11ab3339ddfe2ccafad99a8f9
Paweł Bobryk <bobryk.pawel@gmail.com>
2021-04-27 16:11:50 +0200

    [JAVA] Increment.

--- snippets/tools/mvn.sh
+++ snippets/tools/mvn.sh
@@ -1,3 +1,4 @@
 # Tworzenie projektu
 mvn archetype:generate -DgroupId=com.httpio.app -DartifactId=httpio -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
 mvn archetype:generate -DgroupId=learn.java -DartifactId=modules -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+mvn archetype:generate -DgroupId=repo.java.concurrency.blockingqueu -DartifactId=java-concurrency-blockingqueu -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
