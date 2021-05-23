d78730bf86cdb0dd4e245eb7cf55040c1bdb5fe1
Paweł Bobryk <bobryk.pawel@gmail.com>
2021-05-23 23:36:43 +0200

    Increment.

---
+++ java/ai-deeplearning4j/src/main/java/repo/ai/deeplearning4j/App.java
@@ -0,0 +1,13 @@
+package repo.ai.deeplearning4j;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
