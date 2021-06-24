36a5022f9f342e0c24ba0d37c49a11a261423510
Paweł Bobryk <bobryk.pawel@gmail.com>
2021-06-24 11:57:37 +0200

    Update

---
+++ snippets/tools/date.sh
@@ -0,0 +1,16 @@
+# Wyświetlenie daty
+# %a - Locale’s abbreviated short weekday name (e.g., Mon)
+# %A - Locale’s abbreviated full weekday name (e.g., Monday)
+# %b - Locale’s abbreviated short month name (e.g., Jan)
+# %B - Locale’s abbreviated long month name (e.g., January)
+# %d - Day of month (e.g., 01)
+# %H - Hour (00..23)
+# %I - Hour (01..12)
+# %j - Day of year (001..366)
+# %m - Month (01..12)
+# %M - Minute (00..59)
+# %S - Second (00..60)
+# %u - Day of week (1..7)
+# %Y - Full year (e.g., 2019)
+
+date "+%Y%m%d"
