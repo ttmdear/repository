1aab1bdb0d4fe33ae452f69c22b37a1ed2dc9a01
Paweł Bobryk <bobryk.pawel@gmail.com>
2021-09-15 12:29:45 +0200

    Update

--- snippets/tools/curl.sh
+++ snippets/tools/curl.sh
@@ -1,10 +1,15 @@
-curl -X POST --data 'pass=123' www.test.pl
-
-curl http://192.168.140.2/services/apisrv/installation -H "Host: apps.ut.edu.pl"
-
-# Addig header and -k options couses that certificate is not validate
+# Dodanie nagłówka za pomocą przełącznika -H oraz wyłączenie walidacji
+# certyfikatu za pomocą przełącznika -k
 curl -k -H "Cookie: session_id=fd020cc4-5bc4-45ab-83a0-5e70d6e7c8a4" https://rejestracje.ut.edu.pl/student/exam_registrations?era=7&session_id=fd020cc4-5bc4-45ab-83a0-5e70d6e7c8a4
 
-# Setting CA
+# Wyświetlenie nagłówków za pomocą opcj -I
+curl -I http://localhost:8280/kart/auth/logi
+
+# Ustawienia CA
 curl --cacert /pth/to/my/ca.pem https://url
 curl --header 'Host: www.cyberciti.biz' --cacert /pth/to/my/ca.pem https://207.5.1.10/nixcraft.tar.gz
+
+# Pozostałe przypadki
+curl -X POST --data 'pass=123' www.test.pl
+curl http://192.168.140.2/services/apisrv/installation -H "Host: apps.ut.edu.pl"
+
