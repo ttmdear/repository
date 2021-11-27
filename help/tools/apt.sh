40cbdc973c4793379575c75b29efba07eebcb863
Paweł Bobryk <bobryk.pawel@gmail.com>
2021-11-27 22:59:57 +0100

    Update

---
+++ help/tools/apt.sh
@@ -0,0 +1,11 @@
+# Install previous version
+apt-get install typora=0.11.18-1
+
+# Download package
+apt download typora=0.11.18-1
+
+# See info about package
+apt show typora -a
+
+# See files installed by package
+dpkg -L apache2
