295d27e7761e46a6bf3660cb3e8fcc9d7ad7a46a
Paweł Bobryk <bobryk.pawel@gmail.com>
2021-12-02 23:32:47 +0100

    Update

--- help/tools/mvn.sh
+++ help/tools/mvn.sh
@@ -3,3 +3,21 @@ mvn archetype:generate -DgroupId=com.httpio.app -DartifactId=httpio -DarchetypeA
 mvn archetype:generate -DgroupId=learn.java -DartifactId=modules -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
 mvn archetype:generate -DgroupId=repo.java.concurrency.blockingqueu -DartifactId=java-concurrency-blockingqueu -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
 mvn archetype:generate -DgroupId=repo.java.enum -DartifactId=repo-java-enum-overwritemethod -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
+
+## Run all the unit test classes.
+$ mvn test
+
+## Run a single test class.
+$ mvn -Dtest=TestApp1 test
+
+## Run multiple test classes.
+$ mvn -Dtest=TestApp1,TestApp2 test
+
+## Run a single test method from a test class.
+$ mvn -Dtest=TestApp1#methodname test
+
+## Run all test methods that match pattern 'testHello*' from a test class.
+$ mvn -Dtest=TestApp1#testHello* test
+
+## Run all test methods match pattern 'testHello*' and 'testMagic*' from a test class.
+$ mvn -Dtest=TestApp1#testHello*+testMagic* test
--- help/tools/mysql.sh
+++ help/tools/mysql.sh
@@ -1,1 +1,14 @@
+# Login
 mysql -u usosweb -p -h localhost
+
+# Wgranie bazy danych
+mysql -u root < ofiko.database.sql
+
+# Nazwiazanie połączenia.
+mysql -u yourusername -p yourpassword yourdatabase
+
+# Wykonanie skryptu sql.
+mysql -u yourusername -p yourpassword yourdatabase < text_file
+
+# Wykonanie skryptu z pliku.
+mysql --user="username" --database="databasename" --password="yourpassword" < "filepath"
