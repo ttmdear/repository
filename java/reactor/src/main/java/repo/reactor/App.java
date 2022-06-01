4ef5405aeeae90f53735b4a5844b6fc7a9e95201
Paweł Bobryk <bobryk.pawel@gmail.com>
2022-06-01 16:01:27 +0200

    Update

--- java/reactor/src/main/java/repo/reactor/App.java
+++ java/reactor/src/main/java/repo/reactor/App.java
@@ -1,31 +1,43 @@
 package repo.reactor;
 
-import org.reactivestreams.Subscriber;
-import org.reactivestreams.Subscription;
+import reactor.core.publisher.ConnectableFlux;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 public class App {
     public static void main(String[] args) {
-        Flux.just(1, 2, 3, 4).subscribe(new Subscriber<Integer>() {
-            @Override
-            public void onSubscribe(Subscription subscription) {
-
-            }
-
-            @Override
-            public void onNext(Integer value) {
-                System.out.printf("value " + value);
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-
-            }
-
-            @Override
-            public void onComplete() {
-
+        // Start a cold Publisher which emits 0,1,2 every sec.
+        Flux<Long> flux = Flux.create(sink -> {
+            for (long i = 0; i < 3; i++) {
+                sink.next(i);
             }
         });
+
+        // Make the Publisher Hot
+        ConnectableFlux<Long> connectableFlux = flux.publish();
+
+        // Now that we have a handle on the hot Publisher
+        // Let's subscribe to that with multiple subscribers.
+        connectableFlux.subscribe(i -> System.out.println("first_subscriber received value:" + i));
+
+        // Start firing events with .connect() on the published flux.
+        connectableFlux.connect();
+
+        sleep(3_000);
+        // Let a second subscriber come after some time 3 secs here.
+        connectableFlux.subscribe(i -> System.out.println("second_subscriber received value:" + i));
+    }
+
+    private static void sleep(long mil) {
+        try {
+            Thread.sleep(mil);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void ptid(String m) {
+        System.out.printf(String.format("thread %s - %s\n", Thread.currentThread().getName(), m));
     }
 }
