25f7564a558d5e19a1a369f70cca63e7916719f3
Paweł Bobryk <bobryk.pawel@gmail.com>
2022-06-15 16:00:22 +0200

    Update

---
+++ java/java-concurrency-callable/src/main/java/repo/java-concurrency-callable/App.java
@@ -0,0 +1,13 @@
+package repo.java-concurrency-callable;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
