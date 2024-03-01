b7c670e6121e14568dd7dec2f08ac83c6d1f1bdf
Paweł Bobryk <bobryk.pawel@gmail.com>
2024-03-01 15:08:56 +0100

    sync

---
+++ java/redis/src/main/java/repo/redis/Connect.java
@@ -0,0 +1,36 @@
+package repo.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Connect {
+    public static void main(String[] args) {
+        new Connect().run();
+    }
+
+    public void run() {
+        JedisPool pool = new JedisPool("localhost", 6379);
+
+        try (Jedis jedis = pool.getResource()) {
+            // Store & Retrieve a simple string
+            jedis.set("foo", "bar");
+            System.out.println(jedis.get("foo")); // prints bar
+
+            // Store & Retrieve a HashMap
+            Map<String, String> map = new HashMap<>();
+            map.put("name", "John");
+            map.put("surname", "Smith");
+            map.put("company", "Redis");
+            map.put("age", "29");
+
+            jedis.hset("user-session#123", map);
+
+            System.out.println(jedis.hgetAll("user-session:123"));
+
+            // Prints: {name=John, surname=Smith, company=Redis, age=29}
+        }
+    }
+}
